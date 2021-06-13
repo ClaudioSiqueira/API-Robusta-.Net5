d5aa116ad98999b00866004edb1e1fdf79222ade
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2021-06-13 18:05:38 -0300

    Criando validador de user

--- src/Manager.Domain/Entities/User.cs
+++ src/Manager.Domain/Entities/User.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Manager.Domain.Entities
 {
-	public class User : Base
-	{
-		public User()
-		{
-		}
-		
-	}
+    public class User : Base
+    {
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+        // EF
+        protected User() { }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public override bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+
+    }
 }
 
